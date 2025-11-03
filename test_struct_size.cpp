9b0140f588b252646cec4ff7ee80c56cf895878f
DonggeML <116700814+dongge0210@users.noreply.github.com>
2025-11-03 18:12:09 +0800

    删除无用文件并更新.gitignore以排除新类型的文件

--- test_struct_size.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "src/core/DataStruct/DataStruct.h"
-
-int main() {
-    std::cout << "Size of TemperatureSensor: " << sizeof(TemperatureSensor) << " bytes" << std::endl;
-    std::cout << "Size of SmartDiskScore: " << sizeof(SmartDiskScore) << " bytes" << std::endl;
-    std::cout << "Size of SharedMemoryBlock: " << sizeof(SharedMemoryBlock) << " bytes" << std::endl;
-    return 0;
-}
