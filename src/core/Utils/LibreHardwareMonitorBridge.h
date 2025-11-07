3de26a98509bb8cf7fc7a5f6b52b8ac9c1bac288
dongge0210 <dongge0210@163.com>
2025-11-07 23:47:22 +0800

    "修复项目配置和依赖问题

--- src/core/Utils/LibreHardwareMonitorBridge.h
+++ src/core/Utils/LibreHardwareMonitorBridge.h
@@ -5,15 +5,8 @@
 #include <utility>
 #include <vcclr.h> // 必须包含以使用 gcroot
 
-// DLL引用已在项目文件中正确配置，无需在此重复引用
-// #using "..\src\third_party\LibreHardwareMonitor\bin\Debug\net472\LibreHardwareMonitorLib.dll"
-
-// 添加对 .NET 类型的前向声明
-namespace LibreHardwareMonitor {
-    namespace Hardware {
-        ref class Computer; // 前向声明 Computer
-    }
-}
+// 引用 LibreHardwareMonitor DLL
+#using "..\src\third_party\LibreHardwareMonitor\bin\Debug\net472\LibreHardwareMonitorLib.dll"
 
 // 定义 UpdateVisitor 类
 ref class UpdateVisitor : public LibreHardwareMonitor::Hardware::IVisitor {
